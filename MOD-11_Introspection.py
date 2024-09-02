81b321b1f87f75a6f73f43114705f43565442f3c
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-09-02 11:02:06 +0300

    Add files via upload

--- MOD-11_Introspection.py
+++ MOD-11_Introspection.py
@@ -1,21 +1,33 @@
-
+import inspect
+from pprint import pprint
 # Домашнее задание по теме "Интроспекция"
 
-def introspection_info(obj):
-    # Получаем тип объекта
-    type_name = type(obj).__name__
-    # Получаем атрибуты объекта
-    attributes = dir(obj)
-    # Получаем методы объекта
-    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-    # Получаем модуль, к которому принадлежит объект
-    module = getattr(obj, '__module__', None)
-    return {
-    'type': type_name,
-    'attributes': attributes,
-    'methods': methods,
-    'module': module
-    }
 
-number_info = introspection_info(42)
-print(number_info)
+class Car:
+    def __init__(self, name, model, year):
+        self.name = name
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        return f"{self.name} {self.model} заводит свой двигатель."
+
+    def horn(self):
+        return f"{self.name} издает сигнал Beep! Beep!"
+
+
+def introspection_info(obj):
+    info = {
+        'type': type(obj).__name__,
+        'attributes': [attr for attr in dir(obj) if not attr.startswith('__') and not callable(getattr(obj, attr))],
+        'methods': [meth for meth in dir(obj) if callable(getattr(obj, meth)) and not meth.startswith('__')],
+        'module': inspect.getmodule(introspection_info).__name__,
+    }
+    return info
+
+
+my_car = Car("Toyota", "Camry", 2022)
+print(my_car.start_engine())
+print(my_car.horn())
+car_info = introspection_info(my_car)
+pprint(car_info)
